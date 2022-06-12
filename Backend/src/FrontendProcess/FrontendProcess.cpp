addad8d06cfbcea6e4bac2c4f0d5b8c237a23520
4rtemTrickster <korneev.arty@yandex.ru>
2022-06-13 00:19:06 +0300

    Frontend process moved to class

---
+++ Backend/src/FrontendProcess/FrontendProcess.cpp
@@ -0,0 +1,27 @@
+﻿#include "HSCpch.h"
+#include "FrontendProcess.h"
+
+#include "Log/Log.h"
+
+namespace HSC
+{
+    void FrontendProcess::Init()
+    {
+        if (!FrontendProcessInstance)
+        {
+            HSC_TRACE("Frontend starting up");
+
+            FrontendProcessInstance = ShellExecute(NULL, NULL, exe, NULL, NULL, SW_SHOW);
+
+            if ((INT_PTR)FrontendProcessInstance > 32)
+                HSC_TRACE("Frontend started up successful");
+            else
+            {
+                Error = GetLastError();
+                HSC_ERROR("Frontend process creation failed! Error code: {0}", Error);
+            }
+        }
+        else
+            HSC_INFO("Frontend started up already but Init is called another time!");
+    }
+}
--- Backend/src/Main.cpp
+++ Backend/src/Main.cpp
@@ -1,20 +1,17 @@
 #include "HSCpch.h"
 
+#include "FrontendProcess/FrontendProcess.h"
 #include "Log/Log.h"
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int cmdShow)
 {
     HSC::Log::Init();
-    HINSTANCE FrontendProcess = ShellExecute(NULL, NULL, L"..\\Frontend\\Frontend.exe", NULL, NULL, SW_SHOW);
+    HSC::FrontendProcess::Init();
+    DWORD FrontendProcessStatus = HSC::FrontendProcess::GetStatus();
+    
+    if (!FrontendProcessStatus) return FrontendProcessStatus;
 
-    if ((INT_PTR)FrontendProcess > 32)
-        HSC_TRACE("Frontend startup successful");
-    else
-    {
-        DWORD Error = GetLastError();
-        HSC_ERROR("Frontend process creation failed! Error code: {0}", Error);
-        return Error;
-    }
+
 
     return 0;
 }
